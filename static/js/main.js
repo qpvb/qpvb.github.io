b81049256d4c08a0f9205cef91b9e80d1d464dac
qpvb <132472536+qpvb@users.noreply.github.com>
2023-06-06 14:35:25 +0800

    Delete static directory

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
- document.oncontextmenu = function () { return false; };
-        document.onkeydown = function () {
-            if (window.event && window.event.keyCode == 123) {
-                event.keyCode = 0;
-                event.returnValue = false;
-                return false;
-            }
-        };
